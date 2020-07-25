d1befd0dadaa7c8d5dd5332411f734d6df18d80c
rodolphe37 <rodolphe.a@gmail.com>
2020-07-25 10:49:03 +0200

    add cookies consent module

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './assets/styles/main.scss'
 import Portfolio from '../src/components/portfolio/Portfolio';
+import CookieConsent from "react-cookie-consent";
 // import ReactGA from 'react-ga'
 
 // function initializeReactGA() {
@@ -11,6 +12,25 @@ import Portfolio from '../src/components/portfolio/Portfolio';
 function App() {
   return (
     <div className="App">
+      <CookieConsent
+        location="bottom"
+        buttonText="J'ai compris"
+        cookieName="RodolpheAugustoPortfolio"
+        style={{
+          background: "linear-gradient(90deg, #00aeef 0%, #096fb9 100%)",
+          textShadow: " 2px 2px black",
+          borderTop: "1px solid white"
+        }}
+        buttonStyle={{ background: "#fd7e14", color: "white", fontWeight: "bold", fontSize: "13px", textAlign: "center" }}
+        expires={1}
+        overlay
+      >
+        <span style={{ fontSize: "15px" }}>
+          <b>🍪 Nous utilisons des cookies pour améliorer votre expérience sur notre site 🍪</b>{" "}
+          <br />
+            En continuant votre navigation sur ce site, vous acceptez l’utilisation de cookies pour mesurer l’audience de notre site et vous proposer des fonctionnalités sociales.&nbsp;&nbsp;&nbsp;&nbsp;
+                  </span>
+      </CookieConsent>
       <Portfolio />
     </div>
   );
