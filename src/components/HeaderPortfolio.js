e44295c0ed5bdd4f3c390d8b0a9c99a570635968
rodolphe37 <rodolphe.a@gmail.com>
2020-07-27 22:07:19 +0200

    Work in progress - Translation fr file and refactoring headerPortfolio file

--- src/components/HeaderPortfolio.js
+++ src/components/HeaderPortfolio.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PortfolioMessage from './PortfolioMessage';
 import { useTranslation } from 'react-i18next';
 
 const HeaderPortfolio = () => {
@@ -11,7 +10,7 @@ const HeaderPortfolio = () => {
       <div className="w3-display-left w3-text-white" style={{ padding: '48px' }}>
         <span className="w3-jumbo w3-hide-small">{t('title')}</span><br />
         <span className="w3-xxlarge w3-hide-large w3-hide-medium font-head">{t('title')}.</span><br />
-        <span className="w3-large font-head"><PortfolioMessage /></span>
+        <span className="w3-large font-head">{t('credo')}</span>
         <p><a href="#about" className="  w3-button w3-white w3-padding-large w3-large w3-margin-top w3-opacity w3-hover-opacity-off font-head">{t('titleButton')}</a></p>
       </div>
     </header>
